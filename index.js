1a8eaaee96ec3f69b3a913a264d690b56fb94d49
BondT1 <tombond248@gmail.com>
2023-08-03 21:11:50 +0100

    writeToFile function testing

--- index.js
+++ index.js
@@ -3,23 +3,30 @@ const fs = require("fs");
 
 const { Triangle, Square, Circle } = require("./lib/shapes")
 
-function writeToFile (fileName, answers) {
-    let svg = "";
-    svg = 
-    <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"></svg>
-    svg += "<g>"
-    svg += `${answers.shape}`;
+function writeToFile(fileName, answers) {
+    let string = "";
+    string =
+      '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+    string += "<g>";
+    string += `${answers.shape}`;
 
-    let shapeOption;
-    if (answers.shape === "Circle") {
-        shapeOption = new Circle();
-        svg += `<circle cx="150" cy="100" r="80" fill="${answers.BackgroundColor}"/>`;
+    let shapeChoice;
+    if (answers.shape === "Triangle") {
+      shapeChoice = new Triangle();
+      string += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackground}"/>`;
     } else if (answers.shape === "Square") {
-        shapeOption = new Square();
-        svg += `<rect x="90" y="40" width="120" height="120" fill="${answers.BackgroundColor}"/>`;  
+      shapeChoice = new Square();
+      string += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackground}"/>`;
     } else {
-        shapeOption = new Triangle();
-        svg += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.BackgroundColor}"/>`;
+      shapeChoice = new Circle();
+      string += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackground}"/>`;
     }
-    
+  
+    string += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
+    string += "</g>";
+    string += "</svg>";
+
+    fs.writeFile(fileName, string, (err) => {
+        err ? console.log(err) : console.log("Logo Generated");
+    });
 }
