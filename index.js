9131f932fbdcbc9d0b45a54298d63f1564f353f2
BondT1 <tombond248@gmail.com>
2023-08-03 20:36:43 +0100

    started writeFile function

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const fs = require("fs");
 
 const { Triangle, Square, Circle } = require("./lib/shapes")
 
+function writeToFile (fileName, answers) {
+    let svg = "";
+    svg = 
+    <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"></svg>
+    svg += "<g>"
+    svg += `${answers.shape}`;
+}
