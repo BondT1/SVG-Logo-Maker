09374fc89b109de4161fa0de145c08fab35a7b22
BondT1 <tombond248@gmail.com>
2023-08-06 19:27:41 +0100

    final changes

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 const { Circle, Square, Triangle } = require("./assets/shapes");
-const { CustomConsole } = require("@jest/console");
 
 // writes the file based on user inputs 
 function writeToFile(fileName, answers) {
