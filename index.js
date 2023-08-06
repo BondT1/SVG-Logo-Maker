3c2316a1d9f64c1553deaa02ec0c52b852c21d66
BondT1 <tombond248@gmail.com>
2023-08-06 18:48:49 +0100

    inquirer questions written

--- index.js
+++ index.js
@@ -1,32 +1,77 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-const { Triangle, Square, Circle } = require("./lib/shapes")
+const { Circle, Square, Triangle } = require("./lib/shapes");
+const { CustomConsole } = require("@jest/console");
 
+// writes the file based on user inputs 
 function writeToFile(fileName, answers) {
-    let string = "";
-    string =
-      '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
-    string += "<g>";
-    string += `${answers.shape}`;
+  let string = "";
+  string =
+    '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+  string += "<g>";
+  string += `${answers.shape}`;
 
-    let shapeChoice;
-    if (answers.shape === "Triangle") {
-      shapeChoice = new Triangle();
-      string += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackground}"/>`;
-    } else if (answers.shape === "Square") {
-      shapeChoice = new Square();
-      string += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackground}"/>`;
-    } else {
-      shapeChoice = new Circle();
-      string += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackground}"/>`;
-    }
-  
-    string += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
-    string += "</g>";
-    string += "</svg>";
+  let shapeOption;
+  if (answers.shape === "Triangle") {
+    shapeOption = new Triangle();
+    string += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackground}"/>`;
+  } else if (answers.shape === "Square") {
+    shapeOption = new Square();
+    string += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackground}"/>`;
+  } else {
+    shapeOption = new Circle();
+    string += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackground}"/>`;
+  }
 
-    fs.writeFile(fileName, string, (err) => {
-        err ? console.log(err) : console.log("Logo Generated");
-    });
+  string += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
+  string += "</g>";
+  string += "</svg>";
+
+  fs.writeFile(fileName, string, (err) => {
+      err ? console.log(err) : console.log("Logo Generated");
+  });
 }
+
+// inquirer questions 
+
+function promptUser() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message:
+          "What text do you want to be displayed on the logo? (up to 3 characters)",
+        name: "text",
+      },
+      {
+        type: "input",
+        message:
+          "What text color do you want? (color keyword OR hex)",
+        name: "textColor",
+      },
+      {
+        type: "list",
+        message: "What shape do want your logo to be?",
+        choices: ["Circle", "Square", "Triangle"],
+        name: "shape",
+      },
+      {
+        type: "input",
+        message:
+          "What shape colour do you want? (color keyword OR hex)",
+        name: "shapeBackground",
+      },
+    ])
+    .then((answers) => {
+      if (answers.text.length > 3) {
+        console.log("Value must be no more than 3 characters");
+        promptUser();
+      } else {
+        writeToFile("logo.svg", answers);
+        console.log("Generated logo.svg")
+      }
+    })
+}
+
+promptUser();
